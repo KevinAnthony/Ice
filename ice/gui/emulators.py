d89ca5e2cccd322068c4e69e623637930b287247
Jose Baez <jlb406@gmail.com>
2016-03-20 20:23:54 -0400

    initial porting to PyQt5

--- ice/gui/emulators.py
+++ ice/gui/emulators.py
@@ -4,19 +4,19 @@ emulators.py
 Created by Scott on 2016-03-08.
 Copyright (c) 2016 Kevin Anthony. All rights reserved.
 """
-from PyQt4 import Qt, QtGui, QtCore
+from PyQt5 import Qt, QtWidgets, QtCore
 
-class EmulatoreGui(QtGui.QDialog):
+class EmulatoreGui(QtWidgets.QDialog):
   def __init__(self, parent = None, settings = None):
     super(EmulatoreGui, self).__init__(parent)
     self.app_settings = settings
-    self.buttonBox = QtGui.QDialogButtonBox(self)
+    self.buttonBox = QtWidgets.QDialogButtonBox(self)
     self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-    self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+    self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
 
-    self.textBrowser = QtGui.QTextBrowser(self)
+    self.textBrowser = QtWidgets.QTextBrowser(self)
     self.textBrowser.append("This is a QTextBrowser!")
 
-    self.verticalLayout = QtGui.QVBoxLayout(self)
+    self.verticalLayout = QtWidgets.QVBoxLayout(self)
     self.verticalLayout.addWidget(self.textBrowser)
     self.verticalLayout.addWidget(self.buttonBox)
